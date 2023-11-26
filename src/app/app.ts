5d05bd0c05f2cdffa00df98d0c4c1df56a4f7313
Maria <mariabevenuto062@gmail.com>
2023-11-26 11:44:39 -0300

    tada: initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,15 @@
+import express, {json} from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(json());
+
+
+export default app;
