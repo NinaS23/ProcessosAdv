3ba1957c05ea54064ef54b56eab278858619e761
Maria <mariabevenuto062@gmail.com>
2023-11-30 15:10:37 -0300

    rm: lines spaces

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
-import express, {json} from 'express';
-import 'express-async-errors';
-import cors from 'cors';
-import dotenv from 'dotenv';
-
+const express = require('express');
+const { json } = require('express');
+require('express-async-errors');
+const cors = require('cors');
+const dotenv = require('dotenv');
 
 dotenv.config();
 
 const app = express();
+
 
 app.use(cors());
 app.use(json());
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,4 @@
 import app from './app/app';
-
-
 const port = process.env.PORT || 6003;
 app.listen(port, () => {
 	console.log('Server running on port ' + port);
